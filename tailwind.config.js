725b9707e1c5a5d5322ea6da82d758651b351d4c
Alex <alex@tinkin.one>
2021-09-30 09:50:10 -0500

    Fix tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 const color = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
   theme: {
     colors: {
       transparent: 'transparent',
